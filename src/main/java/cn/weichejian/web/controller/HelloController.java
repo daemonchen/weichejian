9ee4b05f32360d997c0b4597c2db9fa835fc0ecd
daemon <cgyqqcgy@126.com>
2014-02-09 16:47:10 +0800

    init index.jsp

--- src/main/java/cn/weichejian/web/controller/HelloController.java
+++ src/main/java/cn/weichejian/web/controller/HelloController.java
@@ -10,7 +10,7 @@ import com.jfinal.plugin.activerecord.Page;
 public class HelloController extends Controller {
 	public void index() {
 		this.setAttr("test", new String[]{"ji"});
-		this.render("a.jsp");
+		this.render("index.jsp");
 		List<User> users = User.dao.find("select * from t_mem_member where id=?", 10);
 //		User user1 = User.dao.findById(105);
 //		System.out.println(user1.get("email"));
