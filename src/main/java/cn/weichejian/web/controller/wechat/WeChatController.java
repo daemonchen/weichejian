5cc49681e80cc2b006ececa465f966c7d4c243de
daemon <cgyqqcgy@126.com>
2014-03-11 00:18:25 +0800

    rework code for next step

--- src/main/java/cn/weichejian/web/controller/wechat/WeChatController.java
+++ src/main/java/cn/weichejian/web/controller/wechat/WeChatController.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.print.attribute.standard.MediaSize.Other;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+
 import org.jdom.JDOMException;
 
 import com.jfinal.aop.Before;
@@ -29,7 +33,6 @@ public class WeChatController extends Controller {
 		String urlPara = this.getPara(0);
 		//因为要使用urlPara，所以这里不能使用restful接口
 		//TODO make sure this user doesn't bind weixin yet;
-		System.out.println("-------------------" + urlPara);
 		String signature = this.getPara("signature");
 		String timestamp = this.getPara("timestamp");
 		String nonce = this.getPara("nonce");
@@ -38,67 +41,29 @@ public class WeChatController extends Controller {
 		if(null != timestamp && null != nonce && null != echostr && null != signature) {
 			if(WeiXin.access(TOKEN, signature, timestamp, nonce)) {
 				this.renderText(echostr);
-			}
-			else{
-				this.renderText("signature false");
+				return ;
 			}
 		}
 		else{
-			this.renderText("arguments error");
+			this.handlePostRequest(urlPara);
 		}
 		
 		
 	}
-	@ClearInterceptor(ClearLayer.ALL)
-	public void save(){
+	private void handlePostRequest(String userId){
 		try {
-			WxRecvMsg msg = WeiXin.recv(getRequest().getInputStream());
+			ServletInputStream req = getRequest().getInputStream();
+			ServletOutputStream res = getResponse().getOutputStream();
+			WxRecvMsg msg = WeiXin.recv(req);
 			WxSendMsg sendMsg = WeiXin.builderSendByRecv(msg);
 			//TODO use map instead of if else stuff
 			if(msg instanceof WxRecvEventMsg) {
-				WxRecvEventMsg m = (WxRecvEventMsg) msg;
-				String event = m.getEvent();
-				if("subscribe".equals(event)) {
-					String content = "感谢关注";
-					sendMsg = new WxSendTextMsg(sendMsg, content);
-					WeiXin.send(sendMsg, getResponse().getOutputStream());
-				}
+				this.handleEventMsg(msg,sendMsg,res);
 			}
 			
 			if(msg instanceof WxRecvTextMsg) {
-				WxRecvTextMsg m = (WxRecvTextMsg) msg;
+				this.handleKeyWordTextMsg(msg, sendMsg, res);
 				
-				String text = m.getContent();
-				if(null != text) 
-					text = text.trim();
-				
-				Set<String> keyworkds = new HashSet<String>();
-				keyworkds.add("1");
-				keyworkds.add("菜单");
-				keyworkds.add("menu");
-				
-				
-				if(keyworkds.contains(text)) {
-					String content = "1. 菜单\n2.音乐\n,3.图文\n";
-					sendMsg = new WxSendTextMsg(sendMsg, content);
-					
-				} else if("2".equals(text)) {
-					sendMsg = new WxSendMusicMsg(sendMsg, "夜夜夜夜","林志�",
-							"http://hugemaxiao.co/COFFdD0xMzY1Njg5NTIyJmk9MTIxLjM0LjMxLjMmdT1Tb25ncy92MS9mYWludFFDLzQ3LzkxMGFlM2JjMzM5MDNlZGI3NmY3MDUzY2ZjZDI1YjQ3Lm1wMyZtPTMzMWQwNGY2YTNkNTRiNTMwYzc1MjhkN2FmNDRhMGZjJnY9ZG93biZuPdK50rnSudK5JnM9wdbWvuzFJnA9cw==.mp3", 
-							"http://hugemaxiao.co/COFFdD0xMzY1Njg5NTIyJmk9MTIxLjM0LjMxLjMmdT1Tb25ncy92MS9mYWludFFDLzQ3LzkxMGFlM2JjMzM5MDNlZGI3NmY3MDUzY2ZjZDI1YjQ3Lm1wMyZtPTMzMWQwNGY2YTNkNTRiNTMwYzc1MjhkN2FmNDRhMGZjJnY9ZG93biZuPdK50rnSudK5JnM9wdbWvuzFJnA9cw==.mp3");
-				} else if("3".equals(text)) {
-					sendMsg = new WxSendNewsMsg(sendMsg)
-					.addItem("图片4", "描述4", "http://g.hiphotos.baidu.com/album/w%3D230/sign=b2b8791418d8bc3ec60801c9b28ba6c8/1ad5ad6eddc451da5ff2c0c5b7fd5266d016329c.jpg", "http://www.oschina.net")
-					.addItem("图片1", "描述1", "http://a.hiphotos.baidu.com/album/w%3D230/sign=67afac3d242dd42a5f0906a8333a5b2f/8c1001e93901213f088bb78855e736d12f2e952e.jpg", "http://www.google.com")
-					.addItem("图片2", "描述2", "http://e.hiphotos.baidu.com/album/w%3D230/sign=5254e9265243fbf2c52ca120807eca1e/77c6a7efce1b9d1669f2564ff2deb48f8c5464e3.jpg", "http://www.baidu.com")
-					.addItem("图片3", "描述3", "http://d.hiphotos.baidu.com/album/w%3D230/sign=c3b3fe3aaa18972ba33a07c9d6cc7b9d/3812b31bb051f81995bdc164dbb44aed2e73e700.jpg", "http://www.sina.com")
-					;
-				}  else {
-					String content = "1. 菜单\n2.音乐\n,3.图文\n";
-					sendMsg = new WxSendTextMsg(sendMsg, content);
-				}
-				WeiXin.send(sendMsg, getResponse().getOutputStream());
-				return;
 			}  else {
 				String content = "1. 菜单\n2.音乐\n,3.图文\n";
 				sendMsg = new WxSendTextMsg(sendMsg, content);
@@ -110,5 +75,54 @@ public class WeChatController extends Controller {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private void handleEventMsg(WxRecvMsg msg,WxSendMsg sendMsg,ServletOutputStream res) throws JDOMException, IOException{
+		WxRecvEventMsg m = (WxRecvEventMsg) msg;
+		String event = m.getEvent();
+		if("subscribe".equals(event)) {
+			String content = "感谢关注";//TODO select content from t_auto_reply_setting where reply_type=1 and user_id=userId
+			sendMsg = new WxSendTextMsg(sendMsg, content);
+			WeiXin.send(sendMsg, res);
+		}
+	}
+	
+	private void handleKeyWordTextMsg(WxRecvMsg msg,WxSendMsg sendMsg,ServletOutputStream res) throws JDOMException, IOException{
+		WxRecvTextMsg m = (WxRecvTextMsg) msg;
+		String text = m.getContent();
+		if(null != text) 
+			text = text.trim();
+		int msgType = this.getRecvMsgType();
+		/*TODO select key_words from t_auto_reply_setting where user_id=userId, 
+		 * and check the result contain text from request, 
+		otherwise return default content as reply_type is 2.*/
+		switch (msgType) {
+		case 1:
+			String content = "1. 菜单\n2.音乐\n,3.图文\n";
+			sendMsg = new WxSendTextMsg(sendMsg, content);
+			break;
+		case 2:
+			sendMsg = new WxSendMusicMsg(sendMsg, "夜夜夜夜","林志颖",
+					"http://hugemaxiao.co/COFFdD0xMzY1Njg5NTIyJmk9MTIxLjM0LjMxLjMmdT1Tb25ncy92MS9mYWludFFDLzQ3LzkxMGFlM2JjMzM5MDNlZGI3NmY3MDUzY2ZjZDI1YjQ3Lm1wMyZtPTMzMWQwNGY2YTNkNTRiNTMwYzc1MjhkN2FmNDRhMGZjJnY9ZG93biZuPdK50rnSudK5JnM9wdbWvuzFJnA9cw==.mp3", 
+					"http://hugemaxiao.co/COFFdD0xMzY1Njg5NTIyJmk9MTIxLjM0LjMxLjMmdT1Tb25ncy92MS9mYWludFFDLzQ3LzkxMGFlM2JjMzM5MDNlZGI3NmY3MDUzY2ZjZDI1YjQ3Lm1wMyZtPTMzMWQwNGY2YTNkNTRiNTMwYzc1MjhkN2FmNDRhMGZjJnY9ZG93biZuPdK50rnSudK5JnM9wdbWvuzFJnA9cw==.mp3");
+			break;
+		case 3:
+			sendMsg = new WxSendNewsMsg(sendMsg)
+			.addItem("图片4", "描述4", "http://g.hiphotos.baidu.com/album/w%3D230/sign=b2b8791418d8bc3ec60801c9b28ba6c8/1ad5ad6eddc451da5ff2c0c5b7fd5266d016329c.jpg", "http://www.oschina.net")
+			.addItem("图片1", "描述1", "http://a.hiphotos.baidu.com/album/w%3D230/sign=67afac3d242dd42a5f0906a8333a5b2f/8c1001e93901213f088bb78855e736d12f2e952e.jpg", "http://www.google.com")
+			.addItem("图片2", "描述2", "http://e.hiphotos.baidu.com/album/w%3D230/sign=5254e9265243fbf2c52ca120807eca1e/77c6a7efce1b9d1669f2564ff2deb48f8c5464e3.jpg", "http://www.baidu.com")
+			.addItem("图片3", "描述3", "http://d.hiphotos.baidu.com/album/w%3D230/sign=c3b3fe3aaa18972ba33a07c9d6cc7b9d/3812b31bb051f81995bdc164dbb44aed2e73e700.jpg", "http://www.sina.com")
+			;
+			break;
+		default:
+			break;
+		}
+		WeiXin.send(sendMsg, res);
+		return;
+	}
+	
+	private int getRecvMsgType(){
+		//TODO select message_type from t_auto_reply_setting where user_id=userId and key_words=keywords 
+		return 2;
 	}
 }
